68ce572a32d632252717cfb08c124e565294b3b0
thomas.corbeel <thomas.corbeel@flowpilots.com>
2022-08-31 13:54:46 +0200

    FIBS-10 reverted change for measurement serialization for the fromCameraSk method

--- lib/measurement.dart
+++ lib/measurement.dart
@@ -359,8 +359,7 @@ class MeasurementCreationData extends CameraData {
 
   static MeasurementCreationData fromCameraSdk(String cameraMeasurement) {
     var json = jsonDecode(cameraMeasurement);
-    var data = json["measurement"] != null ? jsonDecode(json["measurement"]!) : json;
-    var mCreationData = MeasurementCreationData.fromJson(data);
+    var mCreationData = MeasurementCreationData.fromJson(json);
     return mCreationData;
   }
 
